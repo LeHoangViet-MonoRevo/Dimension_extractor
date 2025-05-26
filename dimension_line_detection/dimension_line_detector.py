f8e5b690dd4f6dbc54fd9aaac79cf24bc241896a
LeHoangViet <lehoangviet@192.168.1.19>
2025-05-26 15:09:44 +0700

    add DimensionLineDetector class as refactorisation

--- dimension_line_detection/dimension_line_detector.py
+++ dimension_line_detection/dimension_line_detector.py
@@ -5,36 +5,29 @@ from typing import List
 import matplotlib.pyplot as plt
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import numpy as np
 import ultralytics
 
 from object_detection_base import BaseYOLO
 
 
-class SectionDetector(BaseYOLO):
+class DimensionLineDetector(BaseYOLO):
     def postprocess(
         self, results: ultralytics.engine.results.Results
     ) -> List[np.ndarray]:
         """
-        Receive the results and then return the detection of cross sections.
+        Receive the cross section and return the detection of dimension lines.
         """
         bbxes = results[0].boxes.data.numpy().tolist()
         orig_image = results.orig_img
-        sections = []
+        dimension_regions = []
         for bbx in bbxes:
             x1, y1, x2, y2, conf_score, cls = bbx
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            sections.append(orig_image[y1:y2, x1:x2, :])
-        return sections, bbxes
+            dimension_regions.append(orig_image[y1:y2, x1:x2, :])
+        return dimension_regions, bbxes
 
 
 if __name__ == "__main__":
-    import cv2
-
-    model = SectionDetector("section_detection_best.pt")
-    image = cv2.imread("84b1b33011a7b22d219aaf627f41868a_2.jpg")
-
-    sections = model.run(image)
-    plt.imshow(sections[0])
-    plt.show()
-    print(f"sections: {sections}")
+    pass
