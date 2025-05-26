adca19f9f9ef377a43e109444189fb06b32a5ac2
LeHoangViet <lehoangviet@192.168.1.19>
2025-05-26 15:00:34 +0700

    add SectionDetector class as a refactorisation

---
+++ section_detection/section_detector.py
@@ -0,0 +1,40 @@
+import os
+import sys
+from typing import List
+
+import matplotlib.pyplot as plt
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import numpy as np
+import ultralytics
+
+from object_detection_base import BaseYOLO
+
+
+class SectionDetector(BaseYOLO):
+    def postprocess(
+        self, results: ultralytics.engine.results.Results
+    ) -> List[np.ndarray]:
+        """
+        Receive the results and then return the detection of cross sections.
+        """
+        bbxes = results[0].boxes.data.numpy().tolist()
+        orig_image = results.orig_img
+        sections = []
+        for bbx in bbxes:
+            x1, y1, x2, y2, conf_score, cls = bbx
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            sections.append(orig_image[y1:y2, x1:x2, :])
+        return sections
+
+
+if __name__ == "__main__":
+    import cv2
+
+    model = SectionDetector("section_detection_best.pt")
+    image = cv2.imread("84b1b33011a7b22d219aaf627f41868a_2.jpg")
+
+    sections = model.run(image)
+    plt.imshow(sections[0])
+    plt.show()
+    print(f"sections: {sections}")
--- section_detection/train.py
+++ section_detection/train.py
@@ -1,13 +1,12 @@
 from ultralytics import YOLO
 
+
 def main():
     # Load a pretrained YOLOv8 model (choose from yolov8n.pt, yolov8s.pt, etc.)
     model = YOLO("yolov8n.pt")
 
-    model.train(data="train_cfg.yml",
-                epochs=5000,
-                imgsz=640,
-                batch=16)
-    
+    model.train(data="train_cfg.yml", epochs=5000, imgsz=640, batch=16)
+
+
 if __name__ == "__main__":
     main()
